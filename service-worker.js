ba6c80d69449aa1f1bbb945bd7d3e1498911b9da
minseolee2005-ui <minseolee2005@gmail.com>
2026-05-04 16:36:58 +0900

    Refactor service worker for caching strategy

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,19 @@
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("app").then(cache => {
-      return cache.addAll(["./", "./index.html"]);
-    })
+const CACHE_NAME = "medical-app-v1";
+const urlsToCache = [
+  "./",
+  "./index.html"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
